ae26ff1e8446b51c3a2db8cabcebf366d7ef0822
villalb4 <villalb4angel@gmail.com>
2023-01-02 23:51:51 -0300

    add: structured and styled component

---
+++ src/components/Quote/Qoute.jsx
@@ -0,0 +1,16 @@
+import './Quote.css'
+
+function Qoute() {
+  return (
+    <div className='qoute_component'>
+      <div className='qoute_divContent'>
+        <div className='quote_rectangle'></div>
+        <div className='quote_divQuote'>
+          <p className='quote_text'>“The first rule of any technology used in a business is that automation applied to an efficient operation will magnify the efficiency. The second is that automation applied to an inefficient operation will magnify the inefficiency.”</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Qoute

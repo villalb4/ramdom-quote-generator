7b5dea0feb5d77eec107f27cfb60207f842e15b9
villalb4 <villalb4angel@gmail.com>
2023-01-02 23:50:46 -0300

    add: Quote component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header/Header';
+import Quote from './components/Quote/Qoute'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Quote />
     </div>
   );
 }

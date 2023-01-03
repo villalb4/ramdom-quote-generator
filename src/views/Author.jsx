b7b792906974714e75741fd0a58299d74e775769
villalb4 <villalb4angel@gmail.com>
2023-01-03 19:46:22 -0300

    add: create and import component

---
+++ src/views/Author.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AllQuotesAuthor from '../components/AllQuotesAuthor/AllQuotesAuthor'
+
+function author() {
+  return (
+    <div>
+      <AllQuotesAuthor />
+    </div>
+  )
+}
+
+export default author

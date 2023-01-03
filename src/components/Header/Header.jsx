3ebf476057cded1c52d0c46b25ca043345f856f4
villalb4 <villalb4angel@gmail.com>
2023-01-03 16:50:08 -0300

    add: dispatch getQuotes thunk

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
-import autorenew from '../../assets/autorenew.svg'
 import './Header.css'
+import autorenew from '../../assets/autorenew.svg'
+import { getQuotes } from '../../redux/thunk/quote'
+import { useDispatch } from 'react-redux'
 
 function Header() {
+
+  const dispatch = useDispatch()
+
+  const handleClick = () => {
+    dispatch(getQuotes())
+  }
+
   return (
     <header>
       <div className='header_divContent'>
-        <button className='header_randomButton'>
+        <button className='header_randomButton' onClick={handleClick}>
           random
           <img className='header_autorenewIcon' src={autorenew} alt="" />
         </button>

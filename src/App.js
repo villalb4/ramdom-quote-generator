1783d50a79f67a5adcca1840cef67cc74ee51032
villalb4 <villalb4angel@gmail.com>
2023-01-03 19:39:48 -0300

    add: Routes react router dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import Home from './views/Home';
+import Author from './views/Author'
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/author' element={<Author />}/>
+      </Routes>
     </div>
   );
 }

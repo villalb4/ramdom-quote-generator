2a2c06d6116cee1d88daad8df5d10243274d1862
villalb4 <villalb4angel@gmail.com>
2023-01-03 19:45:51 -0300

    add: new action thunk allQuotes

--- src/redux/thunk/quote.js
+++ src/redux/thunk/quote.js
@@ -1,9 +1,15 @@
-import { setQuotes } from "../slice/quote";
+import { setQuotes, setAllQuotes } from "../slice/quote";
 
 export const getQuotes = () => {
   return async(dispatch, getState) => {
+    //random quote
     const response = await fetch('https://dummyjson.com/quotes/random')
     const data = await response.json()
     dispatch(setQuotes(data))
+
+    //all quotes
+    const allQuotes = await fetch('https://dummyjson.com/quotes?limit=100')
+    const parse = await allQuotes.json()
+    dispatch(setAllQuotes(parse))
   }
 }

8b008c1b64dbaf64d61810c6ae97d36ef7d5ceb4
villalb4 <villalb4angel@gmail.com>
2023-01-04 10:05:53 -0300

    add: created, structured and styled component

---
+++ src/components/Villalba/Villalba.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Villalba.css'
+
+function Villalba() {
+  return (
+    <div className='villalba_component'>
+      <p className='villalba_cretedBy'>created by <a className='villalba_yo' href="https://github.com/villalb4">villalb4</a> - devChallenges.io</p>
+    </div>
+  )
+}
+
+export default Villalba

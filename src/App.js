690f7a852b8a708fa69d9bff7a1c92a9f7634404
villalb4 <villalb4angel@gmail.com>
2023-01-04 09:54:54 -0300

    add: villalba component & dinamic segment

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Home from './views/Home';
 import Author from './views/Author'
+import Villalba from './components/Villalba/Villalba';
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -8,8 +9,9 @@ function App() {
     <div className="App">
       <Routes>
         <Route path='/' element={<Home />}/>
-        <Route path='/author' element={<Author />}/>
+        <Route path='/author/:name' element={<Author />}/>
       </Routes>
+      <Villalba />
     </div>
   );
 }

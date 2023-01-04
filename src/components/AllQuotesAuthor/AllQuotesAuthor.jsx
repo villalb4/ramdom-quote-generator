64c0edf7e748c33162d636a7bea9ab0631672a8f
villalb4 <villalb4angel@gmail.com>
2023-01-04 10:04:09 -0300

    add: dispatch getAuthorQuotes thunk

--- src/components/AllQuotesAuthor/AllQuotesAuthor.jsx
+++ src/components/AllQuotesAuthor/AllQuotesAuthor.jsx
@@ -1,17 +1,27 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import './AllQuotesAuthor.css'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import { useParams } from 'react-router-dom'
+import { getAuthorQuotes } from '../../redux/thunk/quote'
 
 function AllQuotesAuthor() {
 
-  const authorQuotes = useSelector(e => e.quoteSlice.allQuotes)
+  const dispatch = useDispatch()
+  
+  let { name } = useParams()
+  
+  useEffect(() => {
+    dispatch(getAuthorQuotes(name))
+  }, [name, dispatch])
+  
+  const authorQuotes = useSelector(e => e.quoteSlice.authorQuotes)
 
   return (
     <div className='allQuotesAuthor_component'>
       <div className='allQuotesAuthor_divName'>
-        <span>{authorQuotes[0].author}</span>
+        <span>{authorQuotes[0]?.author}</span>
       </div>
-      {authorQuotes.map((e,i) => {
+      {authorQuotes?.map((e,i) => {
         return(
           <div className='qoute_divContent allQuotesAuthor' key={i}>
             <div className='quote_divQuote'>
